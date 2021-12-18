d5a1624e5a4245d717891693a298dfcbf1b15c77
Jeslin Jayan Varghese <jeslinjaisan@gmail.com>
2021-12-18 15:55:18 +0530

    First Design  Commit

--- src/app/landing-page/landing-page-routing.module.ts
+++ src/app/landing-page/landing-page-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutMainComponent } from './About/about-main/about-main.component';
 import { HomeMainComponent } from './Home/home-main/home-main.component';
 
 const routes: Routes = [
@@ -8,10 +7,6 @@ const routes: Routes = [
     {
       path: '',
       component: HomeMainComponent
-    },
-    {
-      path: 'about',
-      component: AboutMainComponent
     }
   
 ];
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -11,24 +11,7 @@ import { AcademicComponent } from './Offerings/academic/academic.component';
 import { CorporateComponent } from './Offerings/corporate/corporate.component';
 import { PartnershipComponent } from './Offerings/partnership/partnership.component';
 import { HomeMainComponent } from './Home/home-main/home-main.component';
-import { ContactComponent } from './Home/contact/contact.component';
-
-//ABOUT
-import { AboutMainComponent } from './About/about-main/about-main.component';
-import { BannerComponent } from './About/banner/banner.component';
-import { AbtComponent } from './About/abt/abt.component';
-import { FeatureComponent } from './About/feature/feature.component';
-import { LeadersComponent } from './About/leaders/leaders.component';
-import { TeamComponent } from './About/team/team.component';
-import { PatronsComponent } from './About/patrons/patrons.component';
-import { IndustrialPartnersComponent } from './About/industrial-partners/industrial-partners.component';
-import { MarketingPartnersComponent } from './About/marketing-partners/marketing-partners.component';
-import { TrainersComponent } from './About/trainers/trainers.component';
-import { KnowledgePartnersComponent } from './About/knowledge-partners/knowledge-partners.component';
-import { VendorsComponent } from './vendors/vendors.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-
-
 
 
 
@@ -42,20 +25,7 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
     AcademicComponent,
     CorporateComponent,
     PartnershipComponent,
-    HomeMainComponent,
-    ContactComponent,
-    AboutMainComponent,
-    BannerComponent,
-    AbtComponent,
-    FeatureComponent,
-    LeadersComponent,
-    TeamComponent,
-    PatronsComponent,
-    IndustrialPartnersComponent,
-    MarketingPartnersComponent,
-    TrainersComponent,
-    KnowledgePartnersComponent,
-    VendorsComponent,
+    HomeMainComponent
   ],
   imports: [
     CommonModule,
